e06e9bdc6fbe9ffe59398c76a1cf102c1894739b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 12:43:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,29 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+                    class Song
+                        attr_accessor :name, :album, :id
+
+                        def initialize(name, album, id=nil)
+                            @id -id
+                            @name=name
+                            @album=album
+                        end
+
+                        def self. create_table
+                            sql = <<-SQL
+                                CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, name TEXT, album TEXT)
+                                SQL
+                                DB[:conn].execute(sql)
+                            end
+
+                            def save 
+                                sql = <<-SQL
+                                    INSERT INTO songs (name, album)
+                                    VALUES (?,?)
+                                SQL
+
+                                DB[:conn].execute(sql,self.name, self.album)
+                            
+  
 end
