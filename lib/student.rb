7da2749437f6ce55fff65625ac4b8e81f079eed4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 12:51:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,12 +9,12 @@ class Student
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
-    @album= album
+    @grade = grade
   end
 
   def self. create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, name TEXT, album TEXT)
+    CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
     SQL
     DB[:conn].execute(sql)
   end
