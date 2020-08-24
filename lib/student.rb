5a2f4bda439c91a17f14fb0c087ac3f7ef180045
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 12:47:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
-  attr_accessor :name, :album, :id
+  attr_accessor :name, :grade, :id
 
-  def initialize(name, album, id=nil)
+  def initialize(name, grade, id=nil)
     @id -id
-    @name=name
+    @name =name
     @album=album
   end
 
